2e8fa706343eb38f6641769885bb0e47be2b9c3d
AI-Oleg <cosarrtelan@gmail.com>
2023-12-25 17:41:43 +0300

    feat: solve chain sort transform dns file vigenere

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let nameCounts = {};
+  return names.map((name) => {
+    if (nameCounts[name]) {
+      let newName = `${name}(${nameCounts[name]})`;
+      while (nameCounts[newName]) {
+        nameCounts[name]++;
+        newName = `${name}(${nameCounts[name]})`;
+      }
+      nameCounts[newName] = 1;
+      return newName;
+    } else {
+      nameCounts[name] = 1;
+      return name;
+    }
+  });
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
